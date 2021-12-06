b7393c0d12062354b32e466b189c0567a730877f
Дмитрий <dimasvidlov05@gmail.com>
2021-12-07 00:50:19 +0300

    final_version

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-K = int(input("расстояние между остановками: "))
-N = int(input("расстояние пройденное Светланой: "))
-number_station = 1
-distance = K
-previous_station = distance*-1  # предыдущая остановка
-while K < N:
-    K = distance*number_station
-    number_station += 1
-    previous_station += distance
-K -= N  # расстояние от следующей остановки до Светланы
-dist_station = N-previous_station  # расстояние от предыдущей остановки до Светланы
-if K < dist_station or K == dist_station:
-    print(K)
-else:
-    print(dist_station)
+import time
+
+
+def main():
+    distance_stations = int(input("расстояние между остановками: "))
+    distance_sveta = int(input("расстояние пройденное Светланой: "))
+    start_time = time.time()
+    previous_station = distance_sveta - distance_sveta // distance_stations * distance_stations
+    next_station = distance_sveta // distance_stations * distance_stations + distance_stations - distance_sveta
+    print(min(previous_station, next_station))
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
